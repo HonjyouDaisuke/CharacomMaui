f1709fe5b4a45864dfe22879853c476bd95064aa
dasisukehonjyou <honjyou.daisuke@gmail.com>
2025-12-07 16:33:35 +0900

    プロジェクト内の文字選択画面で、選択行がおかしくなる件を対応

---
+++ CharacomMaui.Presentation/Models/CharaDataSummary.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CharacomMaui.Presentation.Models;
+
+public class CharaDataSummary : INotifyPropertyChanged
+{
+  public int Number { get; set; } = 0;
+  public string CharaName { get; set; } = string.Empty;
+  public string MaterialName { get; set; } = string.Empty;
+  public int CharaCount { get; set; } = 0;
+  public int SelectedCount { get; set; } = 0;
+  private bool _isSelected;
+  public bool IsSelected
+  {
+    get => _isSelected;
+    set
+    {
+      if (_isSelected != value)
+      {
+        _isSelected = value;
+        OnPropertyChanged();
+      }
+    }
+  }
+  public event PropertyChangedEventHandler? PropertyChanged;
+  private void OnPropertyChanged([CallerMemberName] string? name = null)
+  {
+    System.Diagnostics.Debug.WriteLine($"CharaDataSummary OnPropertyChanged: {name} for {CharaName}-{MaterialName}");
+    MainThread.BeginInvokeOnMainThread(() =>
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    });
+  }
+}
